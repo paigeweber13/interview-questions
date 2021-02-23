3e67cf92dfd4dc6c2e232f16e8dabd3e288a12ed
Riley Weber <rileyw13@protonmail.com>
2021-02-23 18:42:35 -0500

    moved focus away from testing functions by placing at end of file

--- 0-boilerplate/main.cpp
+++ 0-boilerplate/main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
+
+// function prototypes
+bool testTrue(bool testCase, std::string testName);
+
+template <typename T>
+bool testEqual(T actual, T expected, std::string testName);
+
+
+// interview question functions go here
+
+
+// main
+int main(int argc, char** argv) {
+  std::string helloString = "Hello world!";
+  std::cout << helloString << std::endl;
+  testTrue(helloString > "Hello world", "helloString is greater than hello "
+    "without exclamation point");
+  testEqual(helloString.size(), static_cast<size_t>(12), "helloString has "
+    "size 12");
+  testEqual(helloString, std::string("Hello world!"), "helloString equals "
+    "'Hello world!'.");
+  testEqual(helloString, std::string("Hello world"), "(SHOULD FAIL) "
+    "helloString equals 'Hello world' (without exclamation point).");
+  return 0;
+}
+
+
+// implementations of test functions, not important for interview question
 bool testTrue(bool testCase, std::string testName) {
   if(testCase) {
     std::cout << "PASSED: " << testName << std::endl;
@@ -23,16 +51,3 @@ bool testEqual(T actual, T expected, std::string testName) {
   }
 }
 
-int main(int argc, char** argv) {
-  std::string helloString = "Hello world!";
-  std::cout << helloString << std::endl;
-  testTrue(helloString > "Hello world", "helloString is greater than hello "
-    "without exclamation point");
-  testEqual(helloString.size(), static_cast<size_t>(12), "helloString has "
-    "size 12");
-  testEqual(helloString, std::string("Hello world!"), "helloString equals "
-    "'Hello world!'.");
-  testEqual(helloString, std::string("Hello world"), "(SHOULD FAIL) "
-    "helloString equals 'Hello world' (without exclamation point).");
-  return 0;
-}
